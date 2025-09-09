a98facf4f2a22c30245479574aa408950023a4f1
Unconfirmed2 <eugene.turetsky@gmail.com>
2025-09-09 03:43:17 +0000

    Checkpoint from VS Code for coding agent session

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
-import { Check, ChevronDown, ChevronUp } from "lucide-react"
+import { Check, ChevronUp } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -25,9 +25,7 @@ const SelectTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
-    </SelectPrimitive.Icon>
+    
   </SelectPrimitive.Trigger>
 ))
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
@@ -61,7 +59,7 @@ const SelectScrollDownButton = React.forwardRef<
     )}
     {...props}
   >
-    <ChevronDown className="h-4 w-4" />
+   
   </SelectPrimitive.ScrollDownButton>
 ))
 SelectScrollDownButton.displayName =
